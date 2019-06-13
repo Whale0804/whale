b0320550aef73e9a75a53810d8d942cdb3054080
Githinkcn <549595297@qq.com>
2019-06-13 17:59:42 +0800

    redis jwt 验证登录

--- routers/router.go
+++ routers/router.go
@@ -19,6 +19,7 @@ import (
 
 func init() {
 	//https://blog.csdn.net/mirage003/article/details/87865582
+	//https://github.com/nan1888/beego_jwt
 	var FilterAuth = func(ctx *context.Context) {
 		if flag, _ := beego.AppConfig.Bool("auth"); flag == true {
 			authorization := strings.TrimSpace(ctx.Request.Header.Get("Authorization"))
